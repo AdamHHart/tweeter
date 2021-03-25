1ea59d3fbd92ae4d1eaa261589f04864c76dacb2
Adam Hart <adamhayhart@gmail.com>
2021-03-25 07:02:50 -0700

    twitter loads new tweets, but does not yet post them. They have not yet replaced the hardcoded data array.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -19,28 +19,28 @@
 // }
 
 const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
+  // {
+  //   "user": {
+  //     "name": "Newton",
+  //     "avatars": "https://i.imgur.com/73hZDYK.png"
+  //     ,
+  //     "handle": "@SirIsaac"
+  //   },
+  //   "content": {
+  //     "text": "If I have seen further it is by standing on the shoulders of giants"
+  //   },
+  //   "created_at": 1461116232227
+  // },
+  // {
+  //   "user": {
+  //     "name": "Descartes",
+  //     "avatars": "https://i.imgur.com/nlhLi3I.png",
+  //     "handle": "@rd" },
+  //   "content": {
+  //     "text": "Je pense , donc je suis"
+  //   },
+  //   "created_at": 1461113959088
+  // }
 ]
 
 
@@ -90,18 +90,34 @@ $(document).ready(function () {
       url: url,
       method: "POST",
       data: data,
-    }).then((result) => {
-      console.log("ajax callback called");
-      renderTweets(result);
-    }).catch(err => {
-      console.log("ajax error");
-      console.log(err);
-    });
-  });
+    }).done(function(response) {
+      console.log("Loading your tweets.")
+      loadTweets();
+    })
+    // }).then((result) => {
+    //   console.log("ajax callback called");
+    //   renderTweets(result);
+    // }).catch(err => {
+    //   console.log("ajax error");
+    //   console.log(err);
+    // });
+  })
 
-
-
-  renderTweets(data);
+    const loadTweets = function() {
+      $.ajax({
+        url: "/tweets/",
+        type: "GET",
+        url: "/tweets/",
+        dataType: "JSON"
+      })
+      .done(function(tweets) {
+        console.log("", data);
+        renderTweets(data);
+      });
+  };
+  loadTweets();
+  // renderTweets(data);
+});
   // const tweetElement = createTweetElement(data);
   
   // Test / driver code (temporary)
@@ -114,5 +130,4 @@ $(document).ready(function () {
   
   
   
-});
 
